6daa5762765a709e8864693cca4b3c599058f383
Harold Fachin <harold.f415@gmail.com>
2020-08-05 01:02:44 -0400

    fixed dotenv issue for mailchimp's status code 404 - resources not found / endpoint

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
 
+// HEROKU NAME: rugged-virgin-islands-14550.herokuapp.com
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const https = require("https");
 const fs = require("fs");
+
+require("dotenv").config();
 
 const app = express();
 
@@ -45,11 +48,12 @@ app.post("/", function(req, res) {
 
   const jsonData = JSON.stringify(data);
 
-  const url = "https://usX.api.mailchimp.com/3.0/lists/unique_list_id_goes_here"; // This will come from the main mail-chimp 'endpoint'
+  // Tried to store the entire MailChimp ENDPOINT in a dotenv variable and error occurred. Status code 404 Resource Not Found. Check images for a screenshot of the error
+  const url = "https://" + process.env.REG_ID + ".api.mailchimp.com/3.0/lists/" + process.env.AUD_ID; // This will come from the main mail-chimp 'endpoint'
 
   const options = {
     method: "POST",
-    auth: "user_id:your_unique_user_id_goes_here-usX"
+    auth: process.env.AUTH_KEY
   };
 
   const request = https.request(url, options, function(response) {
@@ -67,7 +71,7 @@ app.post("/", function(req, res) {
     response.on("data", function(data) {
 
       const parseData = JSON.parse(data); // This will turn the json we recieve into a JavaScript Object
-      // console.log(parseData);
+      console.log(parseData);
       // In order to export that data into a jSON file it must not be parsed.
       // fs.writeFile("new_jSON_File.json", data, 'utf8', function(){
       // This is just an empty callback() function to be able to write the json file we recieved into a .json file
